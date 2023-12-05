c8a8cfbfe2f638cdef58b1c81d0531d0f3556dde
Nikita Strygin <nikita6@bk.ru>
2023-12-05 15:38:04 +0300

    Generalize fs tree walking for rom creation

--- shin-rom/src/create.rs
+++ shin-rom/src/create.rs
@@ -12,6 +12,51 @@ use crate::{
     index::{RawEntry, DIRECTORY_OFFSET_MULTIPLIER},
 };
 
+enum DirectoryAction {
+    Enter,
+    // maybe I'll use it later, idk
+    #[allow(unused)]
+    Skip,
+}
+
+trait FsVisitor<F, D> {
+    fn visit_file(&mut self, path: &str, file: &F);
+    fn visit_directory(&mut self, path: &str, directory: &D) -> DirectoryAction;
+}
+
+fn visit_input_fs<'bump, S, V>(root: &InputDirectory<'bump, S>, mut visitor: V) -> V
+where
+    V: FsVisitor<InputFile<S>, InputDirectory<'bump, S>>,
+{
+    fn recur<'bump, S, V>(
+        dir: &InputDirectory<'bump, S>,
+        path_buf: &mut Utf8PathBuf,
+        visitor: &mut V,
+    ) where
+        V: FsVisitor<InputFile<S>, InputDirectory<'bump, S>>,
+    {
+        for (name, entry) in &dir.0 {
+            path_buf.push(name.as_str());
+            match entry {
+                InputEntry::Directory(directory) => {
+                    match visitor.visit_directory(path_buf.as_str(), directory) {
+                        DirectoryAction::Enter => {
+                            recur(directory, path_buf, visitor);
+                        }
+                        DirectoryAction::Skip => {}
+                    }
+                }
+                InputEntry::File(file) => visitor.visit_file(path_buf.as_str(), file),
+            }
+            path_buf.pop();
+        }
+    }
+
+    recur(root, &mut Utf8PathBuf::new(), &mut visitor);
+
+    visitor
+}
+
 pub enum InputEntry<'bump, S> {
     Directory(InputDirectory<'bump, S>),
     File(InputFile<S>),
@@ -97,27 +142,21 @@ impl<'a> FileSource for BaseDirFileSource<'a> {
     }
 }
 
-fn count_directories<S>(input: &InputDirectory<S>) -> usize {
-    input
-        .0
-        .iter()
-        .filter_map(|(_, entry)| match entry {
-            InputEntry::Directory(directory) => Some(count_directories(directory)),
-            InputEntry::File(_) => None,
-        })
-        .sum::<usize>()
-        + 1
+#[derive(Default)]
+struct CountVisitor {
+    pub directory_count: usize,
+    pub file_count: usize,
 }
 
-fn count_files<S>(input: &InputDirectory<S>) -> usize {
-    input
-        .0
-        .iter()
-        .filter_map(|(_, entry)| match entry {
-            InputEntry::Directory(directory) => Some(count_files(directory)),
-            InputEntry::File(_) => Some(1),
-        })
-        .sum::<usize>()
+impl<F, D> FsVisitor<F, D> for CountVisitor {
+    fn visit_file(&mut self, _path: &str, _file: &F) {
+        self.file_count += 1;
+    }
+
+    fn visit_directory(&mut self, _path: &str, _directory: &D) -> DirectoryAction {
+        self.directory_count += 1;
+        DirectoryAction::Enter
+    }
 }
 
 struct Allocator {
@@ -142,93 +181,87 @@ impl Allocator {
     }
 }
 
-fn allocate_directory_recur<'bump, S>(
+struct AllocateDirectoryVisitor<'a, 'bump> {
     rom_encoding: RomEncoding,
-    allocator: &mut Allocator,
-    directory_positions: &mut Vec<(u64, u64)>,
-    directory: &InputDirectory<'bump, S>,
-) {
-    assert!(
-        directory
-            .0
-            .iter()
-            .tuple_windows()
-            .all(|((a, _), (b, _))| a < b),
-        "directory entries must be sorted"
-    );
+    allocator: &'a mut Allocator,
+    directory_positions: &'a mut Vec<'bump, (u64, u64)>,
+}
 
-    allocator.align(DIRECTORY_OFFSET_MULTIPLIER as u64);
+impl<'a, 'bump, S> FsVisitor<InputFile<S>, InputDirectory<'bump, S>>
+    for AllocateDirectoryVisitor<'a, 'bump>
+{
+    fn visit_file(&mut self, _path: &str, _file: &InputFile<S>) {}
 
-    let entries_count = directory.0.len() + 2; // 2 for "." and ".."
-    let entries_size = entries_count * RawEntry::SIZE;
+    fn visit_directory(
+        &mut self,
+        _path: &str,
+        directory: &InputDirectory<'bump, S>,
+    ) -> DirectoryAction {
+        assert!(
+            directory
+                .0
+                .iter()
+                .tuple_windows()
+                .all(|((a, _), (b, _))| a < b),
+            "directory entries must be sorted"
+        );
 
-    let my_offset = allocator.allocate(4 + entries_size as u64);
+        let alloc = &mut self.allocator;
 
-    allocator.allocate(2); // "." entry file name
-    allocator.allocate(3); // ".." entry file name
+        alloc.align(DIRECTORY_OFFSET_MULTIPLIER as u64);
 
-    for (name, _) in &directory.0 {
-        let encoded_name_len = match rom_encoding {
-            RomEncoding::Utf8 => name.len() + 1,
-            RomEncoding::ShiftJIS => {
-                measure_sjis_zstring(name).expect("filename not encodable in Shift-JIS")
-            }
-        };
-        allocator.allocate(encoded_name_len as u64);
-    }
+        let entries_count = directory.0.len() + 2; // 2 for "." and ".."
+        let entries_size = entries_count * RawEntry::SIZE;
 
-    let my_size = allocator.position - my_offset;
+        let my_offset = alloc.allocate(4 + entries_size as u64);
 
-    directory_positions.push((my_offset, my_size));
+        alloc.allocate(2); // "." entry file name
+        alloc.allocate(3); // ".." entry file name
 
-    for (_, entry) in &directory.0 {
-        match entry {
-            InputEntry::Directory(directory) => {
-                allocate_directory_recur(rom_encoding, allocator, directory_positions, directory);
-            }
-            InputEntry::File(_) => {}
+        for (name, _) in &directory.0 {
+            let encoded_name_len = match self.rom_encoding {
+                RomEncoding::Utf8 => name.len() + 1,
+                RomEncoding::ShiftJIS => {
+                    measure_sjis_zstring(name).expect("filename not encodable in Shift-JIS")
+                }
+            };
+            alloc.allocate(encoded_name_len as u64);
         }
+
+        let my_size = alloc.position - my_offset;
+
+        self.directory_positions.push((my_offset, my_size));
+
+        DirectoryAction::Enter
     }
 }
 
-fn allocate_file<'bump, S: FileSource>(
-    allocator: &mut Allocator,
+struct AllocateFileVisitor<'a, 'bump> {
+    allocator: &'a mut Allocator,
     alignment: u64,
-    file_positions: &mut Vec<(u64, u64)>,
-    path_buf: &Utf8Path,
-    file: &InputFile<S>,
-) {
-    allocator.align(alignment);
-
-    let my_offset = allocator.position;
-    let my_size = file
-        .0
-        .size(path_buf.as_str())
-        .expect("Failed to get file size");
-
-    allocator.allocate(my_size);
-
-    file_positions.push((my_offset, my_size));
+    file_positions: &'a mut Vec<'bump, (u64, u64)>,
 }
 
-fn allocate_file_recur<'bump, S: FileSource>(
-    allocator: &mut Allocator,
-    alignment: u64,
-    file_positions: &mut Vec<(u64, u64)>,
-    path_buf: &mut Utf8PathBuf,
-    directory: &InputDirectory<'bump, S>,
-) {
-    for (name, entry) in &directory.0 {
-        path_buf.push(name.as_str());
-        match entry {
-            InputEntry::Directory(directory) => {
-                allocate_file_recur(allocator, alignment, file_positions, path_buf, directory);
-            }
-            InputEntry::File(file) => {
-                allocate_file(allocator, alignment, file_positions, path_buf, file);
-            }
-        }
-        path_buf.pop();
+impl<'a, 'bump, S: FileSource> FsVisitor<InputFile<S>, InputDirectory<'bump, S>>
+    for AllocateFileVisitor<'a, 'bump>
+{
+    fn visit_file(&mut self, path: &str, file: &InputFile<S>) {
+        self.allocator.align(self.alignment);
+
+        let my_offset = self.allocator.position;
+        let my_size = file.0.size(path).expect("Failed to get file size");
+
+        self.allocator.allocate(my_size);
+
+        self.file_positions.push((my_offset, my_size));
+    }
+
+    fn visit_directory(
+        &mut self,
+        _path: &str,
+        _directory: &InputDirectory<'bump, S>,
+    ) -> DirectoryAction {
+        DirectoryAction::Enter
     }
 }
 
@@ -253,17 +286,21 @@ pub fn allocate_index<'bump, S: FileSource>(
         RomVersion::Rom2V0_1 | RomVersion::Rom2V1_1 => RomHeaderV2::SIZE,
     } as u64);
 
-    let directory_count = count_directories(input);
-    let file_count = count_files(input);
+    let CountVisitor {
+        directory_count,
+        file_count,
+    } = visit_input_fs(input, CountVisitor::default());
     let mut directory_positions = Vec::with_capacity_in(directory_count, bump);
 
     let index_offset = allocator.position;
 
-    allocate_directory_recur(
-        rom_version.encoding(),
-        &mut allocator,
-        &mut directory_positions,
+    visit_input_fs(
         input,
+        AllocateDirectoryVisitor {
+            rom_encoding: rom_version.encoding(),
+            allocator: &mut allocator,
+            directory_positions: &mut directory_positions,
+        },
     );
 
     assert_eq!(directory_positions.len(), directory_count);
@@ -273,18 +310,16 @@ pub fn allocate_index<'bump, S: FileSource>(
 
     let mut file_positions = Vec::with_capacity_in(file_count, bump);
 
-    let mut path_buf = Utf8PathBuf::new();
-    allocate_file_recur(
-        &mut allocator,
-        file_offset_multiplier,
-        &mut file_positions,
-        &mut path_buf,
+    visit_input_fs(
         input,
+        AllocateFileVisitor {
+            allocator: &mut allocator,
+            alignment: file_offset_multiplier,
+            file_positions: &mut file_positions,
+        },
     );
 
     allocator.align(file_offset_multiplier);
-
-    drop(path_buf);
 
     assert_eq!(file_positions.len(), file_count);
 
